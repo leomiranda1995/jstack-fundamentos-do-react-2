80ac2e4a09095c19b85888fe0f02e4943242069d
Leonardo da Silva Miranda <leomiranda1995@gmail.com>
2022-09-21 16:29:48 -0300

    Definindo tipos para as props

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Header({ title, children }){
+export default function Header({title, children}){
   return (
     <>
       <h1>{title}</h1>
       {children}
     </>
   );
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node,
+}
+
+Header.defaultProps = {
+  title: `JStack's Blog (propDefault)`
 }

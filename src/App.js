87e4f826207bcec725701c9e843e52b6ded3b17c
Leonardo da Silva Miranda <leomiranda1995@gmail.com>
2022-10-21 18:39:46 -0300

    Trabalhando com States

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Post from './Post';
 import Header from './Header';
 
-const posts = [
-  { id: 1, title: 'Title#01', subtitle: 'Sub#01', likes: 20, },
-  { id: 2, title: 'Title#02', subtitle: 'Sub#02', likes: 10, },
-  { id: 3, title: 'Title#03', subtitle: 'Sub#03', likes: 50, },
-  { id: 4, title: 'Title#04', subtitle: 'Sub#03', likes: 50, },
-]
-
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Title#01', subtitle: 'Sub#01', likes: 20, },
+    { id: 2, title: 'Title#02', subtitle: 'Sub#02', likes: 10, },
+    { id: 3, title: 'Title#03', subtitle: 'Sub#03', likes: 50, },
+    { id: 4, title: 'Title#04', subtitle: 'Sub#03', likes: 50, },
+  ]);
+
+  console.log({ posts });
+
+  function handleRefresh() {
+    setTimeout(() => {
+      setPosts((prevState) => [
+        ...prevState,
+        {
+          id: prevState.length + 1,
+          title: `Title#0${prevState.length + 1}`,
+          subtitle: `Sub#0${prevState.length + 1}`,
+          likes: 50,
+        }
+      ]);
+    }, 2000);
+  }
+
   return (
     <>
       <Header title="JStack's Blog">
-        <h2>Posts da semana</h2>
+        <h2>
+          Posts da semana
+          <button onClick={handleRefresh}>Atualizar</button>
+        </h2>
       </Header>
 
       <hr />

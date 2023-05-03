04c2021c4d62a23aa9feabb1856c917448e97188
Leonardo da Silva Miranda <leomiranda1995@gmail.com>
2023-05-03 19:25:41 -0300

    Estilizando Componentes com Styled

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,7 +4,7 @@ import Post from '../Post';
 import Header from '../Header';
 import { ThemeProvider } from '../../context/ThemeContext';
 
-import styles from './App.scss';
+import { Title } from './styles';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -40,10 +40,10 @@ function App() {
   return (
     <ThemeProvider>
       <Header>
-        <h2 className={styles.title}>
+        <Title as="h2">
           Posts da semana
           <button onClick={handleRefresh}>Atualizar</button>
-        </h2>
+        </Title>
       </Header>
 
       <hr />
